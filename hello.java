04ca5058cc3e4cf9ce9b26beef16d9b779e1609b
DaffaRaditra <daffa_raditra_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:17:22 +0700

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,5 +1,13 @@
 public class hello {
 public static void main(String[] args) {
+    //main method
     System.out.println("Daffa Aqiilah Raditra");
+    System.out.println("SMK Telkom Malang");
+    System.out.println("SMPN 21 Malang");
+    System.out.println("Deni Fardoyo");
+    System.out.println("Rina Rahayu Kusumawati");
+    System.out.println("Daffa Aqiilah Raditra");
+    System.out.println("Polisi");
+    System.out.println("Bermain Sepak Bola");
 }
 }
