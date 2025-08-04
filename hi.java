965944b40a7162fa3f9f0a076edb2c961c043c60
DaffaRaditra <daffa_raditra_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:28:47 +0700

    Add files via upload

---
+++ hi.java
@@ -0,0 +1,46 @@
+public class hi { 
+    public static void main(String[] args) {
+        //Deklarasi variabel
+        String nama = "Mohammad Ibramovic";
+        String usia = "15";
+        String citacita = "Pengusaha";
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Usia: " + usia);
+        System.out.println("Cita-cita: " + citacita);
+
+        String namaKedua = "Revantio Hafiz Prasetyawan";
+        String usiaKedua = "15";
+        String citaCitaKedua = "Bos Muda";
+
+        System.out.println("Nama Kedua: " + namaKedua);
+        System.out.println("Usia Kedua: " + usiaKedua);
+        System.out.println("Cita-cita Kedua: " + citaCitaKedua);
+
+        String namaKetiga = "Muhammad Gavrila Rafif Prayata";
+        String usiaKetiga = "15";
+        String citaCitaKetiga = "Pengusaha";
+
+        System.out.println("Nama Ketiga: " + namaKetiga);
+        System.out.println("Usia Ketiga: " + usiaKetiga);
+        System.out.println("Cita-cita Ketiga: " + citaCitaKetiga);
+
+        String namaKeempat = "Atha Khoirun Ayyasi";
+        String usiaKeempat = "15";
+        String citaCitaKeempat = "Pengusaha Muda";
+
+        System.out.println("Nama Keempat: " + namaKeempat);
+        System.out.println("Usia Keempat: " + usiaKeempat);
+        System.out.println("Cita-cita Keempat: " + citaCitaKeempat);
+
+        String namaKelima = "Sakti Mahaya Zaman";
+        String usiaKelima = "15";
+        String citaCitaKelima = "Sukses";
+
+        System.out.println("Nama Kelima: " + namaKelima);
+        System.out.println("Usia Kelima: " + usiaKelima);
+        System.out.println("Cita-cita Kelima: " + citaCitaKelima);
+
+    }
+    
+}
